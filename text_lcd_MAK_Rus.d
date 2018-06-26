e7f799975b46d6a08714d7e7bb88953cce9dcb5a
lax33 <lax33@tut.by>
2018-06-26 12:11:38 +0300

    beegin

---
+++ text_lcd_MAK_Rus.d
@@ -0,0 +1,164 @@
+text_lcd_MAK_Rus.o: text_lcd_MAK_Rus.c clock.h \
+  D:/Work/Programming/ethernut-5.1/nut/include/dev/board.h \
+  D:/Work/Programming/ethernut-5.1/nutbld/include/cfg/arch.h \
+  D:/Work/Programming/ethernut-5.1/nut/include/cfg/uart.h \
+  D:/Work/Programming/ethernut-5.1/nut/include/compiler.h \
+  D:/Work/Programming/ethernut-5.1/nut/include/toolchain.h \
+  D:/Work/Programming/ethernut-5.1/nut/include/toolchain/gcc.h \
+  D:/Work/Programming/ethernut-5.1/nut/include/toolchain/avrlibc.h \
+  d:/work/programming/avr/winavr-20100110/lib/gcc/../../avr/include/avr/io.h \
+  d:/work/programming/avr/winavr-20100110/lib/gcc/../../avr/include/avr/sfr_defs.h \
+  d:/work/programming/avr/winavr-20100110/lib/gcc/../../avr/include/inttypes.h \
+  d:/work/programming/avr/winavr-20100110/lib/gcc/../../avr/include/stdint.h \
+  d:/work/programming/avr/winavr-20100110/lib/gcc/../../avr/include/avr/iom128.h \
+  d:/work/programming/avr/winavr-20100110/lib/gcc/../../avr/include/avr/portpins.h \
+  d:/work/programming/avr/winavr-20100110/lib/gcc/../../avr/include/avr/common.h \
+  d:/work/programming/avr/winavr-20100110/lib/gcc/../../avr/include/avr/version.h \
+  d:/work/programming/avr/winavr-20100110/lib/gcc/../../avr/include/avr/fuse.h \
+  d:/work/programming/avr/winavr-20100110/lib/gcc/../../avr/include/avr/lock.h \
+  d:/work/programming/avr/winavr-20100110/lib/gcc/../../avr/include/avr/interrupt.h \
+  d:/work/programming/avr/winavr-20100110/lib/gcc/../../avr/include/avr/eeprom.h \
+  d:\work\programming\avr\winavr-20100110\bin\../lib/gcc/avr/4.3.3/include/stddef.h \
+  d:/work/programming/avr/winavr-20100110/lib/gcc/../../avr/include/avr/pgmspace.h \
+  d:/work/programming/avr/winavr-20100110/lib/gcc/../../avr/include/avr/sleep.h \
+  d:/work/programming/avr/winavr-20100110/lib/gcc/../../avr/include/stdlib.h \
+  d:/work/programming/avr/winavr-20100110/lib/gcc/../../avr/include/compat/twi.h \
+  d:/work/programming/avr/winavr-20100110/lib/gcc/../../avr/include/util/twi.h \
+  D:/Work/Programming/ethernut-5.1/nut/include/toolchain/generic.h \
+  D:/Work/Programming/ethernut-5.1/nut/include/arch/avr/board/charon2.h \
+  D:/Work/Programming/ethernut-5.1/nut/include/dev/nicrtl.h \
+  D:/Work/Programming/ethernut-5.1/nut/include/dev/netbuf.h \
+  D:/Work/Programming/ethernut-5.1/nut/include/sys/types.h \
+  D:/Work/Programming/ethernut-5.1/nut/include/net/if_var.h \
+  D:/Work/Programming/ethernut-5.1/nut/include/sys/device.h \
+  D:/Work/Programming/ethernut-5.1/nut/include/sys/file.h \
+  D:/Work/Programming/ethernut-5.1/nut/include/net/if_types.h \
+  D:/Work/Programming/ethernut-5.1/nut/include/dev/usartavr.h \
+  D:/Work/Programming/ethernut-5.1/nut/include/dev/uart.h \
+  D:/Work/Programming/ethernut-5.1/nut/include/dev/usart.h \
+  D:/Work/Programming/ethernut-5.1/nut/include/dev/spibus_avr.h \
+  D:/Work/Programming/ethernut-5.1/nut/include/dev/spibus.h \
+  D:/Work/Programming/ethernut-5.1/nut/include/dev/irqreg.h \
+  D:/Work/Programming/ethernut-5.1/nut/include/dev/irqstack.h \
+  D:/Work/Programming/ethernut-5.1/nut/include/cfg/dev.h \
+  D:/Work/Programming/ethernut-5.1/nut/include/arch/avr/irqreg.h \
+  D:/Work/Programming/ethernut-5.1/nut/include/sys/event.h \
+  D:/Work/Programming/ethernut-5.1/nut/include/sys/thread.h \
+  D:/Work/Programming/ethernut-5.1/nutbld/include/cfg/memory.h \
+  D:/Work/Programming/ethernut-5.1/nut/include/dev/debug.h \
+  d:/work/programming/avr/winavr-20100110/lib/gcc/../../avr/include/string.h \
+  D:/Work/Programming/ethernut-5.1/nut/include/stdio.h \
+  d:\work\programming\avr\winavr-20100110\bin\../lib/gcc/avr/4.3.3/include/stdarg.h \
+  d:/work/programming/avr/winavr-20100110/lib/gcc/../../avr/include/util/delay.h \
+  d:/work/programming/avr/winavr-20100110/lib/gcc/../../avr/include/util/delay_basic.h \
+  text_lcd.h services.h
+
+clock.h:
+
+D:/Work/Programming/ethernut-5.1/nut/include/dev/board.h:
+
+D:/Work/Programming/ethernut-5.1/nutbld/include/cfg/arch.h:
+
+D:/Work/Programming/ethernut-5.1/nut/include/cfg/uart.h:
+
+D:/Work/Programming/ethernut-5.1/nut/include/compiler.h:
+
+D:/Work/Programming/ethernut-5.1/nut/include/toolchain.h:
+
+D:/Work/Programming/ethernut-5.1/nut/include/toolchain/gcc.h:
+
+D:/Work/Programming/ethernut-5.1/nut/include/toolchain/avrlibc.h:
+
+d:/work/programming/avr/winavr-20100110/lib/gcc/../../avr/include/avr/io.h:
+
+d:/work/programming/avr/winavr-20100110/lib/gcc/../../avr/include/avr/sfr_defs.h:
+
+d:/work/programming/avr/winavr-20100110/lib/gcc/../../avr/include/inttypes.h:
+
+d:/work/programming/avr/winavr-20100110/lib/gcc/../../avr/include/stdint.h:
+
+d:/work/programming/avr/winavr-20100110/lib/gcc/../../avr/include/avr/iom128.h:
+
+d:/work/programming/avr/winavr-20100110/lib/gcc/../../avr/include/avr/portpins.h:
+
+d:/work/programming/avr/winavr-20100110/lib/gcc/../../avr/include/avr/common.h:
+
+d:/work/programming/avr/winavr-20100110/lib/gcc/../../avr/include/avr/version.h:
+
+d:/work/programming/avr/winavr-20100110/lib/gcc/../../avr/include/avr/fuse.h:
+
+d:/work/programming/avr/winavr-20100110/lib/gcc/../../avr/include/avr/lock.h:
+
+d:/work/programming/avr/winavr-20100110/lib/gcc/../../avr/include/avr/interrupt.h:
+
+d:/work/programming/avr/winavr-20100110/lib/gcc/../../avr/include/avr/eeprom.h:
+
+d:\work\programming\avr\winavr-20100110\bin\../lib/gcc/avr/4.3.3/include/stddef.h:
+
+d:/work/programming/avr/winavr-20100110/lib/gcc/../../avr/include/avr/pgmspace.h:
+
+d:/work/programming/avr/winavr-20100110/lib/gcc/../../avr/include/avr/sleep.h:
+
+d:/work/programming/avr/winavr-20100110/lib/gcc/../../avr/include/stdlib.h:
+
+d:/work/programming/avr/winavr-20100110/lib/gcc/../../avr/include/compat/twi.h:
+
+d:/work/programming/avr/winavr-20100110/lib/gcc/../../avr/include/util/twi.h:
+
+D:/Work/Programming/ethernut-5.1/nut/include/toolchain/generic.h:
+
+D:/Work/Programming/ethernut-5.1/nut/include/arch/avr/board/charon2.h:
+
+D:/Work/Programming/ethernut-5.1/nut/include/dev/nicrtl.h:
+
+D:/Work/Programming/ethernut-5.1/nut/include/dev/netbuf.h:
+
+D:/Work/Programming/ethernut-5.1/nut/include/sys/types.h:
+
+D:/Work/Programming/ethernut-5.1/nut/include/net/if_var.h:
+
+D:/Work/Programming/ethernut-5.1/nut/include/sys/device.h:
+
+D:/Work/Programming/ethernut-5.1/nut/include/sys/file.h:
+
+D:/Work/Programming/ethernut-5.1/nut/include/net/if_types.h:
+
+D:/Work/Programming/ethernut-5.1/nut/include/dev/usartavr.h:
+
+D:/Work/Programming/ethernut-5.1/nut/include/dev/uart.h:
+
+D:/Work/Programming/ethernut-5.1/nut/include/dev/usart.h:
+
+D:/Work/Programming/ethernut-5.1/nut/include/dev/spibus_avr.h:
+
+D:/Work/Programming/ethernut-5.1/nut/include/dev/spibus.h:
+
+D:/Work/Programming/ethernut-5.1/nut/include/dev/irqreg.h:
+
+D:/Work/Programming/ethernut-5.1/nut/include/dev/irqstack.h:
+
+D:/Work/Programming/ethernut-5.1/nut/include/cfg/dev.h:
+
+D:/Work/Programming/ethernut-5.1/nut/include/arch/avr/irqreg.h:
+
+D:/Work/Programming/ethernut-5.1/nut/include/sys/event.h:
+
+D:/Work/Programming/ethernut-5.1/nut/include/sys/thread.h:
+
+D:/Work/Programming/ethernut-5.1/nutbld/include/cfg/memory.h:
+
+D:/Work/Programming/ethernut-5.1/nut/include/dev/debug.h:
+
+d:/work/programming/avr/winavr-20100110/lib/gcc/../../avr/include/string.h:
+
+D:/Work/Programming/ethernut-5.1/nut/include/stdio.h:
+
+d:\work\programming\avr\winavr-20100110\bin\../lib/gcc/avr/4.3.3/include/stdarg.h:
+
+d:/work/programming/avr/winavr-20100110/lib/gcc/../../avr/include/util/delay.h:
+
+d:/work/programming/avr/winavr-20100110/lib/gcc/../../avr/include/util/delay_basic.h:
+
+text_lcd.h:
+
+services.h:
